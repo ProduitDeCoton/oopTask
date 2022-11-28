a374c1acdbcef87537b45442bc43003159202380
ProduitDeCoton <zalmuxambetov@mail.ru>
2022-11-28 10:04:10 +0500

    task2and3

---
+++ src/Positioned.java
@@ -0,0 +1,19 @@
+/**
+ * Обозначает что объект позиционируемый,
+ * т.е. знает своё местоположение и может его изменить
+ *
+ * @author vpyzhyanov
+ * @since 21.10.2020
+ */
+public interface Positioned {
+
+    /**
+     * Текущее местоположение
+     */
+    Position getPosition();
+
+    /**
+     * Установить текущее местоположение
+     */
+    void setPosition(Position position);
+}
