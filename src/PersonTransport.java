9f88a9b7d2f8a8eb8e81e3bf7954c3833cc003e4
ProduitDeCoton <zalmuxambetov@mail.ru>
2022-12-12 10:15:55 +0500

    task1

--- src/PersonTransport.java
+++ src/PersonTransport.java
@@ -6,5 +6,5 @@ public interface PersonTransport extends Transport {
     /**
      * Доставить человека до пункта назначения
      */
-    public void takeTo(Person person, Position destination);
+    void takeTo(Person person, Position destination);
 }
